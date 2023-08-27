17bf942e4abea8cb60458d69289759e8098e7fa2
Andrew Leonov <andrewleonov999@gmail.com>
2023-08-27 23:13:17 +0300

    Project setup

---
+++ AvitoTestTask/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  AvitoTestTask
+//
+//  Created by Andrew Leonov on 27.08.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
